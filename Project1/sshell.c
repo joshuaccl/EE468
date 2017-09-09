d45755f964f0d79106b3ebbf0eefc1302324fa74
Joshua Lam <jlam890@hawaii.edu>
2017-09-08 14:06:51 -1000

    implemented pipes i think

--- Project1/sshell.c
+++ Project1/sshell.c
@@ -17,132 +17,163 @@
 // #define DEBUG 1  /* In case you want debug messages */
 
 void parse_args(char *buffer, char** args, 
-                size_t args_size, size_t *nargs)
+		size_t args_size, size_t *nargs)
 {
-/* 
- * size_t data type is defined in the 1999 ISO C standard (C99).
- * It is used to represent the sizes of objects. size_t is the
- * preferred way to declare arguments or variables that hold the
- * size of an object.
- */
-    char *buf_args[args_size]; /* You need C99.  Note that args_size
-                                  is normally a constant. */
-    char **cp;  /* This is used as a pointer into the string array */
-    char *wbuf;  /* String variable that has the command line */
-    size_t i, j; 
-    
-    wbuf=buffer;
-    buf_args[0]=buffer; 
-    args[0] =buffer;
-/*
- * Now 'wbuf' is parsed into the string array 'buf_args'
- *
- * The for-loop uses a string.h function
- *   char *strsep(char **stringp, const char *delim);
- *
- *   Description:  
- *   If *stringp = NULL then it returns NULL and does
- *   nothing else.  Otherwise the function finds the first token in
- *   the string *stringp, where tokens are delimited by symbols
- *   in the string 'delim'.  
- *
- *   In the example below, **stringp is &wbu, and 
- *   the delim = ' ', '\n', and '\t'.  So there are three possible 
- *   delimiters. 
- *
- *   So in the string " Aloha World\n", the spaces and "\n" are
- *   delimiters.  Thus, there are three delimiters.  The tokens
- *   are what's between the delimiters.  So the first token is
- *   "", which is nothing because a space is the first delimiter.
- *   The second token is "Aloha", and the third token is "World".
- *   
- *   The function will scan a character string starting from
- *   *stringp, search for the first delimiter.  It replaces
- *   the delimiter with '\0', and *stringp is updated to point
- *   past the token.  In case no delimiter was found, the
- *   token is taken to be the entire string *stringp, and *stringp
- *   is made NULL.   Strsep returns a pointer to the token. 
- *
- *   Example:  Suppose *stringp -> " Aloha World\n"
- *
- *   The first time strsep is called, the string is "\0Aloha World\n",
- *   and the pointer value returned = 0.  Note the token is nothing.
- *
- *   The second time it is called, the string is "\0Aloha\0World\n",
- *   and the pointer value returned = 1  Note that 'Aloha' is a token.
- *
- *   The third time it is called, the string is '\0Aloha\0World\0', 
- *   and the pointer value returned is 7.  Note that 'World' is a token.
- *
- *   The fourth time it is called, it returns NULL.
- *
- *   The for-loop, goes through buffer starting at the beginning.
- *   wbuf is updated to point to the next token, and cp is
- *   updated to point to the current token, which terminated by '\0'.
- *   Note that pointers to tokens are stored in array buf_args through cp.
- *   The loop stops if there are no more tokens or exceeded the
- *   array buf_args.
- */   
-    /* cp is a pointer to buff_args */ 
-    for(cp=buf_args; (*cp=strsep(&wbuf, " \n\t")) != NULL ;){
-        if ((*cp != '\0') && (++cp >= &buf_args[args_size]))
-            break; 
-    }
+	/* 
+	 * size_t data type is defined in the 1999 ISO C standard (C99).
+	 * It is used to represent the sizes of objects. size_t is the
+	 * preferred way to declare arguments or variables that hold the
+	 * size of an object.
+	 */
+	char *buf_args[args_size]; /* You need C99.  Note that args_size
+				      is normally a constant. */
+	char **cp;  /* This is used as a pointer into the string array */
+	char *wbuf;  /* String variable that has the command line */
+	size_t i, j; 
 
-/* 
- * Copy 'buf_args' into 'args'
- */    
-    for (j=i=0; buf_args[i]!=NULL; i++){ 
-        if(strlen(buf_args[i])>0)  /* Store only non-empty tokens */
-            args[j++]=buf_args[i];
-    }
-    
-    *nargs=j;
-    args[j]=NULL;
+	wbuf=buffer;
+	buf_args[0]=buffer; 
+	args[0] =buffer;
+	/*
+	 * Now 'wbuf' is parsed into the string array 'buf_args'
+	 *
+	 * The for-loop uses a string.h function
+	 *   char *strsep(char **stringp, const char *delim);
+	 *
+	 *   Description:  
+	 *   If *stringp = NULL then it returns NULL and does
+	 *   nothing else.  Otherwise the function finds the first token in
+	 *   the string *stringp, where tokens are delimited by symbols
+	 *   in the string 'delim'.  
+	 *
+	 *   In the example below, **stringp is &wbu, and 
+	 *   the delim = ' ', '\n', and '\t'.  So there are three possible 
+	 *   delimiters. 
+	 *
+	 *   So in the string " Aloha World\n", the spaces and "\n" are
+	 *   delimiters.  Thus, there are three delimiters.  The tokens
+	 *   are what's between the delimiters.  So the first token is
+	 *   "", which is nothing because a space is the first delimiter.
+	 *   The second token is "Aloha", and the third token is "World".
+	 *   
+	 *   The function will scan a character string starting from
+	 *   *stringp, search for the first delimiter.  It replaces
+	 *   the delimiter with '\0', and *stringp is updated to point
+	 *   past the token.  In case no delimiter was found, the
+	 *   token is taken to be the entire string *stringp, and *stringp
+	 *   is made NULL.   Strsep returns a pointer to the token. 
+	 *
+	 *   Example:  Suppose *stringp -> " Aloha World\n"
+	 *
+	 *   The first time strsep is called, the string is "\0Aloha World\n",
+	 *   and the pointer value returned = 0.  Note the token is nothing.
+	 *
+	 *   The second time it is called, the string is "\0Aloha\0World\n",
+	 *   and the pointer value returned = 1  Note that 'Aloha' is a token.
+	 *
+	 *   The third time it is called, the string is '\0Aloha\0World\0', 
+	 *   and the pointer value returned is 7.  Note that 'World' is a token.
+	 *
+	 *   The fourth time it is called, it returns NULL.
+	 *
+	 *   The for-loop, goes through buffer starting at the beginning.
+	 *   wbuf is updated to point to the next token, and cp is
+	 *   updated to point to the current token, which terminated by '\0'.
+	 *   Note that pointers to tokens are stored in array buf_args through cp.
+	 *   The loop stops if there are no more tokens or exceeded the
+	 *   array buf_args.
+	 */   
+	/* cp is a pointer to buff_args */ 
+	for(cp=buf_args; (*cp=strsep(&wbuf, " \n\t")) != NULL ;){
+		if ((*cp != '\0') && (++cp >= &buf_args[args_size]))
+			break; 
+	}
+
+	/* 
+	 * Copy 'buf_args' into 'args'
+	 */    
+	for (j=i=0; buf_args[i]!=NULL; i++){ 
+		if(strlen(buf_args[i])>0)  /* Store only non-empty tokens */
+			args[j++]=buf_args[i];
+	}
+
+	*nargs=j;
+	args[j]=NULL;
 }
 
 
 int main(int argc, char *argv[], char *envp[]){
-    char buffer[BUFFER_SIZE];
-    char *args[ARR_SIZE];
+	char buffer[BUFFER_SIZE];
+	char *args[ARR_SIZE];
 
-    int *ret_status;
-    size_t nargs;
-    pid_t pid;
-    
-    while(1){
-        printf("ee468>> "); /* Prompt */
-        fgets(buffer, BUFFER_SIZE, stdin); /* Read in command line */
-              /* Parse the command line into args */
-        parse_args(buffer, args, ARR_SIZE, &nargs); 
+	int *ret_status;
+	size_t nargs;
+	pid_t pid;
+	int p[2];
+	int pipeSeen, i;
+
+	while(1){
+		printf("ee468>> "); /* Prompt */
+		fgets(buffer, BUFFER_SIZE, stdin); /* Read in command line */
+		/* Parse the command line into args */
+		parse_args(buffer, args, ARR_SIZE, &nargs); 
+
+		if (nargs==0) continue; /* Nothing entered so prompt again */
+		if (!strcmp(args[0], "exit" )) exit(0);       
+
+		pipeSeen = 0;
+		
+		for(i = 0; args[i]!=NULL;i++){
+			printf("The command is: %s\n",args[i]);
+		} 	
 	
-	for(int i=0;args[i]!=NULL;i++) {
-		printf("The command is: %s\n",args[i]);
-	}
-        if (nargs==0) continue; /* Nothing entered so prompt again */
-        if (!strcmp(args[0], "exit" )) exit(0);       
-	
-        pid = fork();
+		for (  i = 0 ; args[i]!=NULL ; i++ ) {
+			
+			if(!strcmp(args[i] , "|")) {
 
-        if (pid){  /* The parent */
-#ifdef DEBUG
-            printf("Waiting for child (%d)\n", pid);
-#endif
-            pid = wait(ret_status);
-#ifdef DEBUG
-            printf("Child (%d) finished\n", pid);
-#endif
-        } 
+				pipeSeen = 1;
 
-        else{  /* The child executing the command */
-            if( execvp(args[0], args)) {
-                puts(strerror(errno));
-                exit(127);
-        }
-
-        }
-    }    
-    return 0;
+				//code for piping the data
+				pipe(p);
+				if( fork() == 0) { // this is the child
+					close(stdout);
+					close(p[0]);
+					dup2( p[1], stdout);
+					if(execvp(args[--i], args)) {
+						puts(strerror(errno));
+						exit(127);
+					}
+				}
+				if( fork() == 0) { // this is the child
+					close(stdin);
+					close(p[1]);
+					dup2( p[0], stdin);
+					if(execvp(args[++i], args)) {
+						puts(strerror(errno));
+						exit(127);
+					}
+				}
+				close(p[0]);
+				close(p[1]);
+				
+				wait(ret_status);
+			} 
+				
+			}
+		if(!pipeSeen) {
+			if( fork() == 0) { // this is the child
+			
+				if(execvp(args[0], args)) {
+					puts(strerror(errno));
+					exit(127);
+				}
+				
+			}
+			wait(ret_status);
+		}
+		
+	}    
+	return 0;
 }
 
 
